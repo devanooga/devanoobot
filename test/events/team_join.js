530affe6f61de5828cf9fc1b95809c7146029a0f
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-06-14 19:01:13 -0400

    Flesh out first test for team_join

--- test/events/team_join.js
+++ test/events/team_join.js
@@ -1,6 +1,24 @@
 var should = require('should');
+var sinon = require('sinon');
+require('should-sinon');
+var team_join = require('../../functions/events/team_join');
 
 describe('team_join', () => {
-  it('should respond with 200');
-  it('should send the new member a private message');
+  it('should respond with a message for the member', (done) => {
+    var callback = sinon.spy();
+    var user = {
+      name: 'username'
+    }
+    var event = {}
+
+    team_join(user, null, null, event, botToken = null, callback);
+    callback.should.be.calledOnce();
+    callback.getArgs().args.should.deepEqual([
+      null, {
+        test: `Hello, ${user.name}!`
+      }
+    ]);
+
+    done();
+  });
 });
