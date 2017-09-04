df527d8efe97ee31a6068d7600a28b981efe5017
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-04 09:36:41 -0400

    Test team_join function

---
+++ test/events/team_join.js
@@ -0,0 +1,42 @@
+/* eslint mocha: true */
+require('should')
+const team_join = require('../../events/team_join')
+var event;
+
+describe('events/team_join', () => {
+  beforeEach(() => {
+    event = {
+      user: {
+        name: 'testy_mctestface',
+        profile: {
+          first_name: 'Testy',
+          last_name: 'McTestface',
+        },
+      },
+    }
+  })
+
+  it('should set the username as the channel', done => {
+    const result = team_join(event)
+
+    result.channel.should.equal(event.user.name)
+    done()
+  })
+
+  it('should return a welcome message', done => {
+    const welcome_message = require('../../config/welcome_message')(event.user.profile.first_name)
+    const result = team_join(event)
+
+    result.text.should.equal(welcome_message)
+    done()
+  })
+
+  it('should user the user’s handle if the name is not present', done => {
+    delete event.user.profile.first_name
+    const welcome_message = require('../../config/welcome_message')(event.user.name)
+    const result = team_join(event)
+
+    result.text.should.equal(welcome_message)
+    done()
+  })
+})
