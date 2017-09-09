496edf2b0e77b7499ee1aae1af8a469107bee29a
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-09 14:41:57 -0400

    Promisify fs.appendFile and return promise

--- commands/project/append_to_file.js
+++ commands/project/append_to_file.js
@@ -1,15 +1,14 @@
-var fs = require('fs')
+const {promisify} = require('util')
+const fs = require('fs')
+const appendFile = promisify(fs.appendFile)
 /* Append a file with new line of text.
  *
  * @param {object} data - An object containing the following strings:
  * @param {string} data.filepath - The path to the file that must be amended.
  * @param {string} data.text - The string to append to the end of the file.
- * @param {string} data.username - The Slack username of the submitter.
- * @param {string} [data.fullname] - The full name of the submitter.
  *
- * @throws Will throw an error if document can’t be opened or written to.
+ * @returns A promise that resolves after text has been appended to the file.
  */
-module.exports = data => {
-  await fs.appendFile(data.filepath, data.text, 'utf-8')
-  return
+module.exports = async data => {
+  return appendFile(data.filepath, data.text, 'utf-8')
 }
