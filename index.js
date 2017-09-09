5129b0e2736ffb64d536007bfe61f9a4b7da3efb
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-09 14:13:02 -0400

    Use koa-bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const Koa = require('koa')
 const bot = new Koa()
+const body_parser = require('koa-bodyparser')
 
+bot.use(body_parser())
 bot.use(require('./router').routes())
 
 bot.listen(3000)
