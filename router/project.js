8c20f46aec903bab291a0cc630495fe085142146
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-09 14:46:16 -0400

    Refactor project route middleware

--- router/project.js
+++ router/project.js
@@ -1,5 +1,6 @@
 const winston = require('winston')
 const project = require('../commands/project')
+const path = require('path')
 
 module.exports = async (ctx, next) => {
   // Respond to the response immediately.
@@ -11,15 +12,16 @@ module.exports = async (ctx, next) => {
   const reponame = 'devanooga-meta'
   const repourl = `github.com:devanooga/${reponame}.git`
   const branch = `project-idea-by-${username}-${Date.now()}`
-  const filepath = path.join(tmppath, repo, 'hack-night/projects.md')
-  const text = `ctx.request.body.text - @${username}`
+  const filepath = path.join(tmppath, reponame, 'hack-night/projects.md')
+  const text = `\n- ${ctx.request.body.text} (suggested by @${username})`
   const msg = `Add new project idea by ${username}`
+  const response_url = ctx.request.body.response_url
 
   try {
     await project.checkout_new_branch({ tmppath, reponame, repourl, username, branch })
-    await project.append_to_file({ filepath, text, username })
-    const pull_request = await project.commit_and_push({ msg, branch })
-    await project.report_result({ pull_request, username })
+    await project.append_to_file({ filepath, text })
+    const pull_request = await project.commit_and_push({ tmppath, reponame, msg, branch })
+    await project.report_result({ response_url, pull_request, username })
   }
   catch (error) {
     winston.error(error)
