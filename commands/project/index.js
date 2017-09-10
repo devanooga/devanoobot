af51ac1f53b92af382a83c740c7366dab354ad96
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-09 20:09:47 -0400

    Respond with an attachment instead of just text

--- commands/project/index.js
+++ commands/project/index.js
@@ -19,7 +19,7 @@ module.exports = async (req, res, next) => {
   const branch = `project-idea-by-${username}-${Date.now()}`
   const filepath = path.join(tmppath, reponame, 'hack-night/projects.md')
   const text = `\n- ${req.body.text} (suggested by @${username})`
-  const msg = `Add new project idea by ${username}`
+  const msg = `Add new project idea by @${username}`
   const response_url = req.body.response_url
 
   try {
@@ -28,7 +28,20 @@ module.exports = async (req, res, next) => {
     const pull_request = await commit_and_push({ tmppath, reponame, msg, branch })
 
     const response_body = {
-      text: `I submitted a new project idea on behalf of @${username}: ${pull_request}`,
+      attachments: [
+        {
+          fallback: `I submitted a new project idea on behalf of @${username}: ${pull_request}`,
+          color: '#E0644F',
+          pretext: `I submitted a new project idea on behalf of @${username}.`,
+          author_name: 'devanoobot',
+          title: msg,
+          title_link: pull_request,
+          text: req.body.text,
+          footer: 'github.com/devanooga/devanooga-meta',
+          footer_icon: 'https://www.devanooga.com/images/favicon.png',
+          ts: Math.floor(Date.now() / 1000),
+        }
+      ],
       response_type: 'in_channel',
     }
     await report_result({ response_url, response_body })
