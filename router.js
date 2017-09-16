56bedc45dce2f5f90bf0537cb9574973b3140d02
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-16 17:41:35 -0400

    Route event requests to their handlers correctly
    And handle errors as well.

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.post('/events/:event_type?', require('./events'))
+router.post('/events', require('./events'))
 router.post('/commands/:command_type', require('./commands'))
 
 module.exports = router
