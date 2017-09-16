076bf8b78726ae0f9a4876842bebbf8003a2e218
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-09-16 17:46:51 -0400

    Use a channels object for channel IDs
    This should be fetched dynamically on a regular basis and stored
    in a DB.

--- config/welcome_message.js
+++ config/welcome_message.js
@@ -1,3 +1,4 @@
+const channels = require('./channels')
 /**
  * Produce a welcome message for a new member.
  *
@@ -5,13 +6,13 @@
  * @returns {string} message The welcome message.
  */
 module.exports = name => {
-  let message = `Hello, ${name}. Thanks for joining devanooga!\n\n`
-  message += 'We started this group to give Chattanooga-area tinkerers a place to learn, develop their careers, and socialize with like minded people.\n\n'
-  message += 'We have several channels that you can join that cover a range of topics. Here\'s a few that you might be interested in at first:\n'
-  message += '- #truebegginers: We are all a beginner at something. Come learn something new!\n'
-  message += '- #opportunities: Local (and remote) jobs and contracts.\n'
-  message += '- #hacknight: Learn about our fortnightly hacknights. Join us!\n\n'
-  message += 'If you have any questions, please ask for help in #general. We are (mostly) a friendly group, and would love to help you get started.\n\n'
+  let message = `Hello, <@${name}>. Thanks for joining devanooga! I’m your friendly neighborhood bot.\n\n`
+  message += 'We started this group to give Chattanooga-area tinkerers a place to learn, develop their careers, and socialize with like-minded people.\n\n'
+  message += 'We have several channels covering a range of topics. Here are a few that you might be interested in to get started:\n'
+  message += `- <#${channels.truebeginners}>: We are all a beginner at something. Come learn something new!\n`
+  message += `- <#${channels.opportunities}>: Local (and remote) jobs and contracts.\n`
+  message += `- <#${channels.hacknight}>: Learn about our fortnightly hacknights. Join us!\n\n`
+  message += `If you have any questions, please ask for help in <#${channels.general}>. We are (mostly) a friendly group, and would love to help you get started.\n\n`
   message += 'Thanks again!'
 
   return message
