4dee4b50cd4b914d3d904d746f3744496e3d3816
William Roush <william.roush@roushtech.net>
2017-11-15 20:22:56 -0500

    Set up pm2 config files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR johnny-5@138.197.20.170:devanoobot
-ssh johnny-5@138.197.20.170 ".npm-global/bin/pm2 restart devanoobot"
-
+ssh johnny-5@138.197.20.170 "cd devanoobot && ../.npm-global/bin/pm2 restart devanoobot.config.json --update-env"
