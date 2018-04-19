c45f2a73cb8aee3a65aa06c6fead034011686316
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2018-04-18 20:16:33 -0400

    Fix deploy script to avoid nesting

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR johnny-5@138.197.20.170:devanoobot
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR johnny-5@138.197.20.170:
 ssh johnny-5@138.197.20.170 "cd devanoobot && npm run migrations:prod && ../.npm-global/bin/pm2 restart devanoobot.config.json --update-env"
