4cda7d47bb0cf34b7d5119fcc89009a3bfd3e5f0
Thomas Krause <thomas_krause_it@outlook.com>
2018-05-16 21:00:54 -0400

    rework shitty code, add test that may or may not pass

--- commands/cryptovalue/index.js
+++ commands/cryptovalue/index.js
@@ -1,30 +1,25 @@
 const winston = require('winston')
+const got = require('got')
+const url = 'https://api.coinmarketcap.com/v1/ticker/?convert=usd'
 
 module.exports = async (req, res, next) => {
-	try {
-		var input = req.body.text || 'bitcoin';
-		$.getJSON(
-			'https://api.coinmarketcap.com/v1/ticker/?convert=usd', 
-			function(data) {
-				var results = data.filter(function(d) {
-					return d.id === input.toLowerCase() || d.symbol.toLowerCase() === input.toLowerCase();
-				});
-				if(results.length === 1) {
-					var crypto = results[0];
-					res.send({
-						text: crypto.name + '(' + crypto.symbol + ') - $' + crypto.price_usd + '(USD)',
-						response_type: 'in_channel',
-					});
-				} else {
-					res.send({
-						text: 'Could not find crypto currency with the name: ' + input,
-						response_type: 'ephemeral',
-					});
-				}
-			}
-		);
-	}
-	catch (error) {
-		winston.error(error)
-	}
+  const input = req.body.text || 'bitcoin';
+  return got(url).then((data) => {
+    const results = data.filter( (item) => {
+      return item.id === input.toLowerCase() || item.symbol.toLowerCase() === input.toLowerCase();
+    )};
+    
+    if(results.length === 1) {
+      var crypto = results[0];
+      res.send({
+        text: crypto.name + '(' + crypto.symbol + ') - $' + crypto.price_usd + '(USD)',
+        response_type: 'in_channel',
+      });
+    } else {
+      res.send({
+        text: 'Could not find crypto currency with the name: ' + input,
+        response_type: 'ephemeral',
+      });
+    };
+  }).catch(winston.error);
 };
