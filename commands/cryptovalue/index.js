7501fdd5efa1803e9a3afc90f842b47eb3778c08
Thomas Krause <thomas_krause_it@outlook.com>
2018-06-27 20:15:58 -0400

    change return value to be sent to proper slack response url

--- commands/cryptovalue/index.js
+++ commands/cryptovalue/index.js
@@ -7,26 +7,33 @@ module.exports = (req, res, next) => {
 
   return got(url).then(raw => {
     res.send({
-        text: 'Can do! Querying...',
-        response_type: 'ephemeral',
+      text: 'Can do! Querying...',
+      response_type: 'ephemeral',
     })
-    
+
+    const response_url = req.body.response_url
     const response = JSON.parse(raw.body)
     const results = response.filter( (item) => {
       return item.id === input.toLowerCase() || item.symbol.toLowerCase() === input.toLowerCase()
     })
 
+    var response_body
+
     if(results.length === 1) {
       var crypto = results[0]
-      res.send({
+      response_body = {
         text: crypto.name + '(' + crypto.symbol + ') - $' + crypto.price_usd + '(USD)',
         response_type: 'in_channel',
-      })
+      }
     } else {
-      res.send({
+      response_body = {
         text: 'Could not find crypto currency with the name: ' + input,
         response_type: 'ephemeral',
-      })
+      }
     }
+
+    return got.post(response_url, {
+      body: JSON.stringify(response_body),
+    })
   }).catch(winston.error)
 }
